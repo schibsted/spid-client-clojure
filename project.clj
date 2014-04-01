4babe007403409eee446e96c3ac2080774dd7d29
Magnar Sveen <magnars@gmail.com>
2014-04-01 13:43:29 +0200

    Release 0.2.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject spid-sdk-clojure "0.2.0"
+(defproject spid-sdk-clojure "0.2.1"
   :description "A thin clojure wrapper around SPiD Java SDK"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.4"]
-                 [no.spp.sdk/spp-sdk "1.3.10"]])
+                 [no.spp.sdk/spp-sdk "1.3.11"]])
