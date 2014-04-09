b899b27ea7503bdcd6d1abaef11011eb91e2784f
Christian Johansen <christian@cjohansen.no>
2014-04-09 10:18:08 +0200

    Use a proper null

--- src/spid_sdk_clojure/raw.clj
+++ src/spid_sdk_clojure/raw.clj
@@ -8,7 +8,9 @@
 (defn- mapify-response [response]
   {:body (.getResponseBody response)
    :status (.getHttpResponseCode response)
-   :error (.getError response)
+   ;; Can't use (.getError response), because it will return "null" (a string)
+   ;; most of the time
+   :error (:error (.getJsonContainer response))
    :data (and (.getData response) (json-parse (.getData response)))
    :success? (<= 200 (.getHttpResponseCode response) 299)})
 
