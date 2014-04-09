c3839dc0e52469bb9a21ef1ac2b3f99583c60794
Christian Johansen <christian@cjohansen.no>
2014-04-09 08:09:20 +0200

    Raw endpoints return maps

--- src/spid_sdk_clojure/raw.clj
+++ src/spid_sdk_clojure/raw.clj
@@ -1,13 +1,19 @@
 (ns spid-sdk-clojure.raw)
 
+(defn mapify-response [response]
+  {:body (.responseBody response)
+   :status (.httpResponseCode response)
+   :error (.error response)
+   :data (.data response)})
+
 (defn GET [client endpoint & [parameters]]
-  (-> client (.GET endpoint (or parameters {}))))
+  (-> client (.GET endpoint (or parameters {})) mapify-response))
 
 (defn POST [client endpoint parameters]
-  (-> client (.POST endpoint parameters)))
+  (-> client (.POST endpoint parameters) mapify-response))
 
 (defn PUT [client endpoint parameters]
-  (-> client (.PUT endpoint parameters)))
+  (-> client (.PUT endpoint parameters) mapify-response))
 
 (defn DELETE [client endpoint]
-  (-> client (.DELETE endpoint)))
+  (-> client (.DELETE endpoint) mapify-response))
