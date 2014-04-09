23f446ee31c1bf1fb10971589bf3da6c9600ab8e
Christian Johansen <christian@cjohansen.no>
2014-04-09 09:19:17 +0200

    API errors aren't exceptions

--- src/spid_sdk_clojure/raw.clj
+++ src/spid_sdk_clojure/raw.clj
@@ -1,14 +1,29 @@
 (ns spid-sdk-clojure.raw
-  (:require [clojure.data.json :as json]))
+  (:require [clojure.data.json :as json])
+  (:import no.spp.sdk.exception.SPPClientResponseException))
 
-(defn mapify-response [response]
+(defn- json-parse [data]
+  (json/read-str data :key-fn keyword))
+
+(defn- mapify-response [response]
   {:body (.getResponseBody response)
    :status (.getHttpResponseCode response)
    :error (.getError response)
-   :data (json/read-str (.getData response) :key-fn keyword)})
+   :data (and (.getData response) (json-parse (.getData response)))})
+
+(defn- mapify-error [error]
+  {:body (.getResponseBody error)
+   :status (.getHttpResponseCode error)
+   :error (:error (json-parse (.getResponseBody error)))})
+
+(defmacro request [forms]
+  `(try
+     (mapify-response (~@forms))
+     (catch SPPClientResponseException e#
+       (mapify-error e#))))
 
 (defn GET [client endpoint & [parameters]]
-  (-> client (.GET endpoint (or parameters {})) mapify-response))
+  (request (.GET client endpoint (or parameters {}))))
 
 (defn POST [client endpoint parameters]
   (-> client (.POST endpoint parameters) mapify-response))
