7b4b96371f4c07b045a11ab6e1dfceba72efa705
Christian Johansen <christian@cjohansen.no>
2014-04-23 10:09:41 +0200

    Up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spid-sdk-clojure "0.4.0"
+(defproject spid-sdk-clojure "0.4.1"
   :description "A thin clojure wrapper around SPiD Java SDK"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.4"]
