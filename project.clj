76f14994f6e2e511b509fdcc10a66b391c2027d1
Claes Bergsten <claes.bergsten@schibstedpayment.no>
2014-05-09 16:44:51 +0200

    Upped spid api client version

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "A thin clojure wrapper around SPiD Java client"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.4"]
-                 [no.spid/no.spid.api.client "1.0"]])
+                 [no.spid/no.spid.api.client "1.2"]])
